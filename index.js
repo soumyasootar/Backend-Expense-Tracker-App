146731991095183a366eee60fecf2454f748f987
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-04-15 21:59:20 +0530

    some changes

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ server.use(router);
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
+
+// import express from 'express';
+// import low from 'lowdb';
+// import FileSync from 'lowdb/adapters/FileSync';
+
+// const adapter = new FileSync('db.json');
+// const db = low(adapter);
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.use(express.json());
+
+// app.get('/users', (req, res) => {
+//   const users = db.get('users').value();
+//   res.json(users);
+// });
+
+// app.post('/users', (req, res) => {
+//   const user = req.body;
+//   db.get('users').push(user).write();
+//   res.json(user);
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
