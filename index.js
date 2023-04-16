621907a2c18fe7d96a9aeceb006c9ef349c35518
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-04-16 16:23:23 +0530

    .

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
-const PORT = process.env.PORT || 4000;
-
+const PORT = process.env.PORT || 4000 ;
 server.use(middlewares);
 server.use(router);
-
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running port ${PORT}`);
 });
